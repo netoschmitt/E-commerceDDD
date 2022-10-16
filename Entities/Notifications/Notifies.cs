35cca6af0e4f065e6dc251f14b6e232ce58be4bb
neto schmitt <agneloschmittneto@gmail.com>
2022-10-16 18:20:01 -0300

    propriedade notifies

---
+++ Entities/Notifications/Notifies.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Notifications
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            Notitycoes = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notitycoes { get; set; }
+
+        // validaçao todos campos/obj no dominio
+        public bool ValidaPropriedadeString(string valor, string nomepropriedade)
+        {
+            if(string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomepropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Campo obrigatório",
+                    NomePropriedade = nomepropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        // validaçao prop int/obj no dominio
+        public bool ValidaPropriedadeInt(int valor, string nomepropriedade)
+        {
+            if (valor > 1 || string.IsNullOrWhiteSpace(nomepropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Valor deve ser maior que 0",
+                    NomePropriedade = nomepropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        // validaçao campos decimal no dominio
+        public bool ValidaPropriedadeDecimal(decimal valor, string nomepropriedade)
+        {
+            if (valor > 1 || string.IsNullOrWhiteSpace(nomepropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Valor deve ser maior que 0",
+                    NomePropriedade = nomepropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
