a55f2e81fb02f7bfcc7fd92af312af82139edb64
neto schmitt <agneloschmittneto@gmail.com>
2022-10-17 18:02:14 -0300

    Infra context

---
+++ Infraestrutura/Configuration/ContextBase.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestrutura.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+        
+        // setar a string de conexão 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConnectionConfig()
+        {
+            string StrCon = "Server=NETO;Database=Db_E-commerce;User Id=sa;Password=123456";
+                return StrCon;
+        }
+    }
+}
