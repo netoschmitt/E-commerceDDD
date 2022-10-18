03219f938d180dd1c592380b29882d681e336a88
neto schmitt <agneloschmittneto@gmail.com>
2022-10-18 19:32:26 -0300

    Tabela Produto

--- Entities/Entities/Produto.cs
+++ Entities/Entities/Produto.cs
@@ -15,15 +15,45 @@ namespace Entities.Entities
         public int Id { get; set; }
 
         [Column("PRD_NOME")]
+        [MaxLength(255)]
         [Display(Name = "Nome")]
         public string Nome { get; set; }
+        
+        [Column("PRD_DESCRICAO")]
+        [MaxLength(150)]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Column("PRD_OBSERVACAO")]
+        [MaxLength(2000)]
+        [Display(Name = "Observação")]
+        public string Observacao { get; set; }
 
         [Column("PRD_VALOR")]
         [Display(Name = "Valor")]
         public decimal Valor { get; set; }
 
+        [Column("PRD_QTD_ESTOQUE")]
+        [Display(Name = "Quantidade Estoque")]
+        public int QtdEstoque { get; set; }
+
+        [Display(Name = "Usuário")]
+        [ForeignKey("ApplicationUser")]
+        [Column(Order =1)]
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
         [Column("PRD_ESTADO")]
         [Display(Name = "Estado")]
         public bool Estado { get; set; }
+
+        [Column("PRD_DATA_CADASTRO")]
+        [Display(Name ="Data de Cadastro")]
+        public DateTime DataCadastro { get; set; }
+
+        [Column("PRD_DATA_ALTERACAO")]
+        [Display(Name = "Data de Alteração")]
+        public DateTime DataAlteracao { get; set; }
     }
 }
